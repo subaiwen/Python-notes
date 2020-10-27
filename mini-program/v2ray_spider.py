50718f27acfb4db11334e312dba421c370cf8928
subaiwen <32149556+subaiwen@users.noreply.github.com>
2020-10-27 16:52:28 +0800

    v2ray_spider: enable mutiple url to scrape

--- mini-program/v2ray_spider.py
+++ mini-program/v2ray_spider.py
@@ -1,15 +1,31 @@
 import requests
 import re
 
-url = 'https://github.com/bannedbook/fanqiang/wiki/v2ray免费账号' #v2ray
-url2 = 'https://github.com/bannedbook/fanqiang/' #翻墙信息网
+#v2ray
+url_list = ['https://github.com/bannedbook/fanqiang/wiki/v2ray免费账号', 
+            'https://github.com/iwxf/free-v2ray']
 
-res = requests.get(url)
-content = res.text
+def spider(url):
+    '''
+    url --> list of sub-link
+    :param: url
+    :return: v2ray_list: list of sub-link
+    '''
+    res = requests.get(url)
+    content = res.text
 
-pattern = '<p>vmess.+?</p>'
-v2ray_url = re.findall(pattern, content) # re.search() return the first occurence
-v2ray = [re.sub('<.+?>', '', i) for i in v2ray_url]
+    pattern = 'vmess://.+?[^<]+'
+    v2ray_url = re.findall(pattern, content) # re.search() return the first occurence
+    v2ray_list = [re.sub('<.+?>', '', i) for i in v2ray_url]
 
-for i in v2ray:
-    print('\n' + i)
+    return v2ray_list
+
+sub_link_list = []
+for url in url_list:
+    v2ray_list = spider(url)
+    for v2ray in v2ray_list:
+        sub_link_list = sub_link_list + [i for i in re.split('(\n)', v2ray) if i != '\n'] # remove '\n'
+        # remove an unexist element will raise error
+    
+for sub_link in sub_link_list:
+    print('\n' + sub_link)
