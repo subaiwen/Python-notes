fbbe784ad2990f4b232b6246eb046f5d48dc3b03
subaiwen <32149556+subaiwen@users.noreply.github.com>
2020-10-27 17:09:55 +0800

    v2ray_spider: restruture

--- mini-program/v2ray_spider.py
+++ mini-program/v2ray_spider.py
@@ -7,25 +7,26 @@ url_list = ['https://github.com/bannedbook/fanqiang/wiki/v2ray免费账号',
 
 def spider(url):
     '''
-    url --> list of sub-link
+    scrpe list of sub-link from a url
     :param: url
-    :return: v2ray_list: list of sub-link
+    :return: sub_list: list of sub-links
     '''
     res = requests.get(url)
     content = res.text
 
     pattern = 'vmess://.+?[^<]+'
-    v2ray_url = re.findall(pattern, content) # re.search() return the first occurence
-    v2ray_list = [re.sub('<.+?>', '', i) for i in v2ray_url]
-
-    return v2ray_list
+    v2ray_list = re.findall(pattern, content) # re.search() return the first occurence
+    v2ray_list = [re.sub('<.+?>', '', i) for i in v2ray_list]
+    sub_list = []
+    # apply when a str contains links split by \n
+    for v2ray_str in v2ray_list:
+        v2ray_str_list = [i for i in re.split('(\n)', v2ray_str) if i != '\n'] # remove an unexist element will raise error
+        sub_list = sub_list + v2ray_str_list
+    return sub_list
 
 sub_link_list = []
 for url in url_list:
-    v2ray_list = spider(url)
-    for v2ray in v2ray_list:
-        sub_link_list = sub_link_list + [i for i in re.split('(\n)', v2ray) if i != '\n'] # remove '\n'
-        # remove an unexist element will raise error
+    sub_link_list = sub_link_list + spider(url)
     
 for sub_link in sub_link_list:
     print('\n' + sub_link)
